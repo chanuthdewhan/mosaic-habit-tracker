16b74bf3a79f438ab30f266b815191f6d431f0e5
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-01-31 23:19:22 +0530

    chore: configure Tailwind CSS with NativeWind V4

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Mosiac</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Mosaic!
+      </Text>
     </View>
   );
 }
