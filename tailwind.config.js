f3233c2f6c8ec62ccc06c46df562e42e8275f353
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-02-02 01:09:48 +0530

    feat: configure splash screen and app icons setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f48c25",
+        "background-light": "#f8f7f5",
+        "background-dark": "#121212",
+      },
+      fontFamily: {
+        regular: ["Inter-Regular", "sans-serif"],
+        medium: ["Inter-Medium", "sans-serif"],
+        semibold: ["Inter-SemiBold", "sans-serif"],
+        bold: ["Inter-Bold", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "0.5rem",
+        lg: "0.75rem",
+        xl: "1.5rem",
+        full: "9999px",
+      },
+    },
   },
   plugins: [],
 };
